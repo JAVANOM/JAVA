840c8765f1d02c97b9d1d40ca2ea8334cca7e37c
geon <lkcbap12@gmail.com>
2024-08-17 10:51:35 +0900

    token commit

---
+++ src/main/java/ch11/StringTokenizerEx5.java
@@ -0,0 +1,26 @@
+package ch11;
+
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class StringTokenizerEx5 {
+    public static void main(String[] args) {
+
+        String data = "100,,,200,300";
+        String[] result = data.split(",");
+        System.out.println(Arrays.toString(result));
+
+        StringTokenizer st = new StringTokenizer(data, ",");
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i] + "|");
+        }
+
+        System.out.println("개수:"+result.length);
+
+        int i=0;
+        for(;st.hasMoreTokens();i++)
+            System.out.print(st.nextToken()+"|");
+
+        System.out.print("개수:"+i);
+    }//main
+}
